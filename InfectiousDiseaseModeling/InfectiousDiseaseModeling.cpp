8401f66d3c0b714313fcc5ee6202c0066ff46940
temp3rr0r <madks@yahoo.gr>
2015-12-31 17:00:24 +0100

    Added boost library support, for the future usage of the boost graph subset. Also openmp 2.0 works normally on VS 2015

--- InfectiousDiseaseModeling/InfectiousDiseaseModeling.cpp
+++ InfectiousDiseaseModeling/InfectiousDiseaseModeling.cpp
@@ -1,6 +1,19 @@
 #include <omp.h>
 #include <iostream>
 
+#include <boost/lambda/lambda.hpp>
+#include <iterator>
+#include <algorithm>
+
+void main2()
+{
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+}
+
 int main() {
 
 	bool do_use_parallel = false;
@@ -12,8 +25,8 @@ int main() {
 	{
 		std::cout << "Hello from " << omp_get_thread_num() << std::endl;
 	}
-
-
+	
+	main2();
 
 	system("pause");	
 }
