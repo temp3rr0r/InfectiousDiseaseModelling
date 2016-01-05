b612df43d4596ebda2d3ed71038748a3ba701938
temp3rr0r <madks@yahoo.gr>
2016-01-05 04:11:00 +0100

    Default settings.

--- InfectiousDiseaseModeling/Settings.h
+++ InfectiousDiseaseModeling/Settings.h
@@ -6,17 +6,17 @@
 // S means selector
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> LocationUndirectedGraph;
 
-static const bool SAVE_CSV = true;
+static const bool SAVE_CSV = false;
 static const bool SAVE_GRAPHVIZ = false;
-static const bool SHOW_EPIDEMIC_RESULTS = true;
+static const bool SHOW_EPIDEMIC_RESULTS = false;
 
 static const int DEFAULT_NUMBER_OF_THREADS = 4;
 
-static const std::uint8_t DEFAULT_TOTAL_EPOCHS = 60;
-static const int DEFAULT_INDIVIDUAL_COUNT = 400;
+static const std::uint8_t DEFAULT_TOTAL_EPOCHS = 30;
+static const int DEFAULT_INDIVIDUAL_COUNT = 1000;
 
 static const int INITIAL_INFECTED_COUNT = 15;
 
-static const std::uint8_t DEFAULT_REPEAT_COUNT = 10;
+static const std::uint8_t DEFAULT_REPEAT_COUNT = 4;
 
 static const int CHUNK_SIZE_DIVIDER = 10;
